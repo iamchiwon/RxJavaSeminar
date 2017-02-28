3f1b36273a553235c9be283e605c16304a717d23
Song Chiwon <iamchiwon@gmail.com>
2017-02-28 18:43:08 +0900

    Polling 예제

--- app/src/main/java/com/iamchiwon/seminar/rxjavaseminar/MainActivity.java
+++ app/src/main/java/com/iamchiwon/seminar/rxjavaseminar/MainActivity.java
@@ -1,47 +1,60 @@
 package com.iamchiwon.seminar.rxjavaseminar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.Switch;
+import android.widget.Button;
+import android.widget.TextView;
 
-import com.iamchiwon.seminar.rxjavaseminar.rxcomponent.RxButtonWrapper;
+import java.util.concurrent.TimeUnit;
 
+import rx.Observable;
 import rx.Subscription;
-import rx.functions.Action1;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    Subscription eventCapturer;
+    int pollingCount = 0;
+    Subscription pollingSubscription;
+
+    TextView pollingCountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RxButtonWrapper rxButtonWrapper = new RxButtonWrapper(this, R.id.rxwrapped_button);
+        pollingCountTextView = (TextView) findViewById(R.id.polling_count);
 
-        //
-        // RxButton 이벤트 처리
-        final Action1<View> action = v -> Log.d("RxSeminar", "Button Clicked");
-        eventCapturer = rxButtonWrapper.rxClick().subscribe(action);
-
-        //
-        // Switch 이벤트 처리
-        Switch eventSwitch = (Switch) findViewById(R.id.event_switch);
-        eventSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    eventCapturer = rxButtonWrapper.rxClick().subscribe(action);
-
-                } else {
-                    eventCapturer.unsubscribe();
-
-                }
-            }
+        Button secondButton = (Button) findViewById(R.id.show_second);
+        secondButton.setOnClickListener(v -> {
+            startActivity(new Intent(this, SecondActivity.class));
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        polling();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (pollingSubscription != null) {
+            pollingSubscription.unsubscribe();
+        }
+    }
+
+    private void polling() {
+        pollingSubscription = Observable.interval(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.computation())
+                .map(time -> {
+                    pollingCount += 1;
+                    return "Polling-" + pollingCount;
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(text -> pollingCountTextView.setText(text));
     }
 }
---
+++ app/src/main/java/com/iamchiwon/seminar/rxjavaseminar/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.iamchiwon.seminar.rxjavaseminar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by iamchiwon on 2017. 2. 28..
+ */
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
