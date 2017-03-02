9e943d29e34704b1a79b38689fe502d3c148cd2e
Song Chiwon <iamchiwon@gmail.com>
2017-03-02 14:33:06 +0900

    EditText 의 valid 체크 예제

--- app/src/main/java/com/iamchiwon/seminar/rxjavaseminar/MainActivity.java
+++ app/src/main/java/com/iamchiwon/seminar/rxjavaseminar/MainActivity.java
@@ -1,60 +1,58 @@
 package com.iamchiwon.seminar.rxjavaseminar;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import com.jakewharton.rxbinding.view.RxView;
+import com.jakewharton.rxbinding.widget.RxTextView;
 
 import java.util.concurrent.TimeUnit;
 
-import rx.Observable;
-import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
-
-    int pollingCount = 0;
-    Subscription pollingSubscription;
-
-    TextView pollingCountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pollingCountTextView = (TextView) findViewById(R.id.polling_count);
+        TextView resultText = (TextView) findViewById(R.id.valid_result);
+        EditText editText = (EditText) findViewById(R.id.input_field);
 
-        RxView.clicks(findViewById(R.id.show_second)).subscribe(v -> {
-            startActivity(new Intent(this, SecondActivity.class));
-        });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        polling();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (pollingSubscription != null) {
-            pollingSubscription.unsubscribe();
-        }
-    }
-
-    private void polling() {
-        pollingSubscription = Observable.interval(1, TimeUnit.SECONDS)
-                .subscribeOn(Schedulers.computation())
-                .map(time -> {
-                    pollingCount += 1;
-                    return "Polling-" + pollingCount;
-                })
+        RxTextView.textChanges(editText)
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .map(charSequence -> charSequence.toString())
+                .distinct()
+                .map(text -> checkValid(text.toString()))  //expensive
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(text -> pollingCountTextView.setText(text));
+                .subscribe(valid -> {
+                    if (valid) {
+                        resultText.setTextColor(Color.GREEN);
+                        resultText.setText("Valid");
+                    } else {
+                        resultText.setTextColor(Color.RED);
+                        resultText.setText("Invalid");
+                    }
+                }, error -> error.printStackTrace());
+
+    }
+
+    private boolean checkValid(String text) {
+        try {
+
+            //expensive job
+            Thread.sleep(1000);
+            Log.d("RxSeminar", "checkValid on " + Thread.currentThread().getName());
+
+            int num = Integer.parseInt(text);
+            return (num >= 1 && num <= 100);
+
+        } catch (Exception e) {
+        }
+        return false;
     }
 }
