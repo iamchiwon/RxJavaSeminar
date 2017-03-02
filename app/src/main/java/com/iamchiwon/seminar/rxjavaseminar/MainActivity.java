b1e2ffe65dd4b2a821a4a35408e1058e76f91bba
Song Chiwon <iamchiwon@gmail.com>
2017-03-02 15:00:33 +0900

    password checker

--- app/src/main/java/com/iamchiwon/seminar/rxjavaseminar/MainActivity.java
+++ app/src/main/java/com/iamchiwon/seminar/rxjavaseminar/MainActivity.java
@@ -3,17 +3,24 @@ package com.iamchiwon.seminar.rxjavaseminar;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.text.TextUtils;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.jakewharton.rxbinding.view.RxView;
 import com.jakewharton.rxbinding.widget.RxTextView;
 
-import java.util.concurrent.TimeUnit;
-
-import rx.android.schedulers.AndroidSchedulers;
+import rx.Observable;
 
 public class MainActivity extends AppCompatActivity {
+
+    enum PasswordCheckResult {
+        Empty,
+        Short,
+        NotEqual,
+        Equal,
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,38 +28,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView resultText = (TextView) findViewById(R.id.valid_result);
-        EditText editText = (EditText) findViewById(R.id.input_field);
+        EditText password = (EditText) findViewById(R.id.password_field);
+        EditText reinput = (EditText) findViewById(R.id.reinput_field);
+        Button submit = (Button) findViewById(R.id.submit_button);
 
-        RxTextView.textChanges(editText)
-                .debounce(500, TimeUnit.MILLISECONDS)
-                .map(charSequence -> charSequence.toString())
-                .distinct()
-                .map(text -> checkValid(text.toString()))  //expensive
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(valid -> {
-                    if (valid) {
-                        resultText.setTextColor(Color.GREEN);
+        Observable<String> passwordObservable =
+                RxTextView.textChanges(password).map(chseq -> chseq.toString());
+
+        Observable<String> reinputObservable =
+                RxTextView.textChanges(reinput).map(chseq -> chseq.toString());
+
+        Observable
+                .combineLatest(passwordObservable
+                        , reinputObservable
+                        , (pass, repass) -> {
+                            if (TextUtils.isEmpty(pass)) return PasswordCheckResult.Empty;
+                            if (pass.length() < 6) return PasswordCheckResult.Short;
+
+                            if (TextUtils.isEmpty(repass)) return PasswordCheckResult.Empty;
+
+                            if (pass.equals(repass)) return PasswordCheckResult.Equal;
+                            return PasswordCheckResult.NotEqual;
+                        })
+                .doOnNext(check -> {
+                    if (check == PasswordCheckResult.Equal) {
                         resultText.setText("Valid");
-                    } else {
+                        resultText.setTextColor(Color.GREEN);
+                    } else if (check == PasswordCheckResult.NotEqual) {
+                        resultText.setText("Not Same");
                         resultText.setTextColor(Color.RED);
-                        resultText.setText("Invalid");
+                    } else if (check == PasswordCheckResult.Short) {
+                        resultText.setText("Need more inputs");
+                        resultText.setTextColor(Color.BLUE);
+                    } else if (check == PasswordCheckResult.Empty) {
+                        resultText.setText("No Input");
+                        resultText.setTextColor(Color.DKGRAY);
+                    } else {
+                        resultText.setText("");
                     }
-                }, error -> error.printStackTrace());
-
-    }
-
-    private boolean checkValid(String text) {
-        try {
-
-            //expensive job
-            Thread.sleep(1000);
-            Log.d("RxSeminar", "checkValid on " + Thread.currentThread().getName());
-
-            int num = Integer.parseInt(text);
-            return (num >= 1 && num <= 100);
-
-        } catch (Exception e) {
-        }
-        return false;
+                })
+                .map(check -> check == PasswordCheckResult.Equal)
+                .subscribe(RxView.enabled(submit));
     }
 }
